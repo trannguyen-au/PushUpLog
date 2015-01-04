67e2d1de34b875ea23302bf9057b1d6dffe4eaa9
Wery Nguyen <nguyennt86@gmail.com>
2015-01-05 08:50:29 +1100

    First commit

---
+++ app/src/main/java/mobile/wnext/pushupslog/models/PracticeLog.java
@@ -0,0 +1,11 @@
+package mobile.wnext.pushupslog.models;
+
+/**
+ * Created by Wery7 on 4/1/2015.
+ */
+public class PracticeLog {
+    private int id;
+    private String logDate;
+    private int countPushUps;
+    private int countTime; // time in seconds
+}
