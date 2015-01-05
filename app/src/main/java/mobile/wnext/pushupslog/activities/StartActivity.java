1048eb7ed978854cea1879a7978baa29b5791003
Wery Nguyen <nguyennt86@gmail.com>
2015-01-06 08:02:45 +1100

    update button style & training activity

--- app/src/main/java/mobile/wnext/pushupslog/activities/StartActivity.java
+++ app/src/main/java/mobile/wnext/pushupslog/activities/StartActivity.java
@@ -1,20 +1,36 @@
 package mobile.wnext.pushupslog.activities;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import mobile.wnext.pushupslog.R;
 
 
-public class StartActivity extends ActionBarActivity {
+public class StartActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button btnStartTraining, btnLog, btnPractice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        btnStartTraining = (Button) findViewById(R.id.btnStartTraining);
+        btnLog = (Button) findViewById(R.id.btnLog);
+        btnPractice = (Button) findViewById(R.id.btnPractice);
+
+        btnStartTraining.setOnClickListener(this);
+        btnLog.setOnClickListener(this);
+        btnPractice.setOnClickListener(this);
     }
 
 
@@ -38,5 +54,19 @@ public class StartActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnStartTraining) {
+            Intent trainingIntent = new Intent(getApplicationContext(), TrainingActivity.class);
+            startActivity(trainingIntent);
+        }
+        else if(view == btnPractice) {
+
+        }
+        else if(view == btnLog) {
+
+        }
     }
 }
